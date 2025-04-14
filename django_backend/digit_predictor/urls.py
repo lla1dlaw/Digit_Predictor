f869a29a3005d6fc59adc002a3b4cf414cc4bb2e
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-04-14 14:36:52 -0400

    configured landing page to be index.html

--- django_backend/digit_predictor/urls.py
+++ django_backend/digit_predictor/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('digit_predictor/', views.test_view, name='Test View'),
+    path('', views.main, name='Predictor'),
 ]
--- django_backend/digit_predictor/views.py
+++ django_backend/digit_predictor/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def test_view(request):
+def main(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
