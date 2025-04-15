a267ce1a2c8e6ad6557a85cacfbd1478ba572003
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-04-14 20:52:09 -0400

    didn't change anythin (redundant commit)

--- django_backend/digit_predictor/views.py
+++ django_backend/digit_predictor/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
+
 def main(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
