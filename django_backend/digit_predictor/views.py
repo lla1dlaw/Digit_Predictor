5c11873558a3c83b468bd2eb1343899693148fe7
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-04-15 15:55:57 -0400

    need to implement restful API

--- django_backend/digit_predictor/views.py
+++ django_backend/digit_predictor/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from utils.Model_Loader import Loader
 
 
 def main(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
-# Create your views here.
